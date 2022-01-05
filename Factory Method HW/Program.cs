6fd8c6f83bdeea1cdea0640602029268edb4d95e
rayron5656 <86421133+rayron5656@users.noreply.github.com>
2022-01-05 22:28:59 +0200

    Add files via upload

---
+++ Factory Method HW/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factory_Method_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            OuterConstactedC C = new OuterConstactedC(); //Can't create since its private
+            C = OuterConstactedC.Create();               // have to use method
+        }
+    }
+
+    public class OuterConstactedC
+    {
+        private OuterConstactedC()
+        {
+
+        }
+        public static OuterConstactedC Create()
+        {
+            return new OuterConstactedC();
+        }
+    }
+    
+}
